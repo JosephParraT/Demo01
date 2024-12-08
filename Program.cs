4376c9a46d183373d0fa6a3dd3d7055ace3567cc
JosephParraT <Josephpt2004@outlook.com>
2024-12-08 11:42:40 -0500

    Sesion 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Demo_01
+﻿using MyLibrary;
+
+namespace Demo_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.Write("Hello Writer");
+            //Console.WriteLine("Hello, World!");
         }
     }
 }
